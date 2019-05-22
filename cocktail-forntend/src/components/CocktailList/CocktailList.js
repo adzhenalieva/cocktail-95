d1387733edcb1e40b8a91752bdfe8596c65f532c
Aisalkyn Dzhenalieva <aidzhenalieva@gmail.com>
2019-05-22 16:30:40 +0600

    Cocktails.js container created for main info, components coctail list and UI Toolbar created

---
+++ cocktail-forntend/src/components/CocktailList/CocktailList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import CocktailThumbnail from "../CocktailThumbnail/CocktailThumbnail";
+import {Card, CardBody} from "reactstrap";
+
+const CocktailList = props => {
+    return (
+        <Card className="mb-5" >
+            <CardBody>
+                <p><strong >
+                    {props.title}
+                </strong></p>
+                <CocktailThumbnail image={props.image}/>
+                <p><strong>Recipe</strong> <br/>
+                {props.recipe}</p>
+                <p><strong>Ingredients</strong></p>
+                {props.ingredients.map(ingr => (
+                    <p
+                        key={ingr._id}>
+
+                        <span>{ingr.name},</span>
+                        <span> amount: {ingr.amount}</span>
+                    </p>
+                ))}
+                <p>{props.published}</p>
+            </CardBody>
+        </Card>
+    );
+};
+
+export default CocktailList;
